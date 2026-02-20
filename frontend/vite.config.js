a4a3c19eacece77ead418c97ed42b0bfdecc973a
Rinpyre <thecutefireboy@gmail.com>
2026-02-20 16:51:39 +0100

    feat(vite): Add path aliasing configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      '@renderer': resolve('src'),
+      '@components': resolve('src/components'),
+      '@routes': resolve('src/routes'),
+      '@assets': resolve('src/assets'),
+      '@utils': resolve('utils')
+    }
+  },
   plugins: [react(), tailwindcss()]
 })
