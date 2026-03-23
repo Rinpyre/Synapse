5443b6aee06e50bbc406f029f0f8cecd1b6e8807
Owl <balazs200428@gmail.com>
2026-03-23 20:19:26 +0100

    feat(layout): Update css on AppLayout and add new content-view window

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -1,7 +1,7 @@
 export default function Topbar() {
     return (
-        <div className="topbar" style={{width: '100%', height: '64px'}}>
-            Synapse
+        <div className="topbar flex justify-start items-center w-full h-10.5">
+            <h1 className="text-snow text-lg font-bold ml-1.75">Synapse</h1>
         </div>
     )
 }
