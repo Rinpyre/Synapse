5af55e8b4dc2c51371f60e7ff4ad142f7bea2f5c
Owl <balazs200428@gmail.com>
2026-03-23 23:01:18 +0100

    feat(layout): Implement LogTable component

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }) {
             <div className="main-content flex-1 flex flex-col">
                 <Topbar />
                 <div className="flex w-full grow justify-center overflow-hidden">
-                    <div className="content-view bg-secondary flex w-7xl h-180 rounded-4xl">
+                    <div className="content-view bg-secondary flex w-7xl h-180 rounded-4xl" >
                         {children}
                     </div>
                 </div>
