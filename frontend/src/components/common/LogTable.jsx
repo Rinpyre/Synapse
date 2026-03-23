8b5d308ea6a6bd3e6c038745d097522329d337d3
Owl <balazs200428@gmail.com>
2026-03-23 23:01:18 +0100

    feat(layout): Implement LogTable component

---
+++ frontend/src/components/common/LogTable.jsx
@@ -0,0 +1,90 @@
+export default function LogTable() {
+    return (
+        <div className="log-table w-full p-3">
+            {/* bordered, striped, full-width, centered text, has separating lines*/}
+            <table className="table table-bordered table-striped w-full text-center text-snow">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Timestamp</th>
+                        <th scope="col">Actor</th>
+                        <th scope="col">Entity Type</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>35517</td>
+                        <td>2026-01-27T12:47:05.4666667</td>
+                        <td>183</td>
+                        <td>StudentProfilePage</td>
+                        <td>Student shown</td>
+                    </tr>
+                    <tr>
+                        <td>35516</td>
+                        <td>2026-01-27T12:46:50.2833333</td>
+                        <td>183</td>
+                        <td>StudentProfilePage</td>
+                        <td>Student shown</td>
+                    </tr>
+                    <tr>
+                        <td>35515</td>
+                        <td>2026-01-27T12:46:40.0100000</td>
+                        <td>183</td>
+                        <td>SchoolProfilePage</td>
+                        <td>School shown</td>
+                    </tr>
+                    <tr>
+                        <td>35514</td>
+                        <td>2026-01-27T12:46:29.8866667</td>
+                        <td>183</td>
+                        <td>SchoolProfilePage</td>
+                        <td>School shown</td>
+                    </tr>
+                    <tr>
+                        <td>35513</td>
+                        <td>2026-01-27T13:45:39.4000000</td>
+                        <td>183</td>
+                        <td>LoginSystem</td>
+                        <td>Niklas SpeedAdmin ApS speedadmin medarbetare har loggat in. (10.200.36.50)</td>
+                    </tr>
+                    <tr>
+                        <td>35512</td>
+                        <td>2026-01-27T12:38:40.7733333</td>
+                        <td>60</td>
+                        <td>StudentProfilePage</td>
+                        <td>Student shown</td>
+                    </tr>
+                    <tr>
+                        <td>35511</td>
+                        <td>2026-01-27T12:24:49.9333333</td>
+                        <td>178</td>
+                        <td>LoginService</td>
+                        <td>Henning Primdahl has logged in. Represented by Casper Koch Albæk SpeedAdmin ApS</td>
+                    </tr>
+                    <tr>
+                        <td>35510</td>
+                        <td>2026-01-27T12:24:47.0133333</td>
+                        <td>178</td>
+                        <td>GuardianProfilePage</td>
+                        <td>Parent/guardian shown</td>
+                    </tr>
+                    <tr>
+                        <td>35509</td>
+                        <td>2026-01-27T12:20:29.0566667</td>
+                        <td>178</td>
+                        <td>LoginService</td>
+                        <td>Pim Kaysergracht has logged in. Represented by Casper Koch Albæk SpeedAdmin ApS</td>
+                    </tr>
+                    <tr>
+                        <td>35508</td>
+                        <td>2026-01-27T12:20:27.3300000</td>
+                        <td>178</td>
+                        <td>StudentProfilePage</td>
+                        <td>Student shown</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
