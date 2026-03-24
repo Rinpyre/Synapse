bc50e962ec20a8b073634ccdeb7e64fae77a18a1
Owl <balazs200428@gmail.com>
2026-03-24 12:15:51 +0100

    feat(layout): Update AppLayout children issue

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -5,13 +5,9 @@ export default function AppLayout({ children }) {
     return (
         <div className="app bg-primary flex h-screen w-full">
             <Sidebar />
-            <div className="main-content flex-1 flex flex-col">
+            <div className="main-content grow flex flex-col">
                 <Topbar />
-                <div className="flex w-full grow justify-center overflow-hidden">
-                    <div className="content-view bg-secondary flex w-7xl h-180 rounded-4xl" >
-                        {children}
-                    </div>
-                </div>
+                {children}
             </div>
         </div>
     )
