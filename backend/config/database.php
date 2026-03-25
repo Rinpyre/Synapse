26bfd7104e451592f86839ce35cad3bc9c614edf
Ktoto198 <frybenderer43@gmail.com>
2026-03-25 08:25:42 +0100

    chore(backend):configure database connection.

--- backend/config/database.php
+++ backend/config/database.php
@@ -107,10 +107,10 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => '', 
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
         ],
 
     ],
