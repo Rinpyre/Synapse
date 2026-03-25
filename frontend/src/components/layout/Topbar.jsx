119385626abb3aaf4b7c08047d7ef007fa1c8799
Owl <balazs200428@gmail.com>
2026-03-25 19:54:06 +0100

    feat(layout): Update in accordance to feedback

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -1,4 +1,4 @@
-export default function Topbar() {
+export function Topbar() {
     return (
         <div className="topbar flex justify-start items-center w-full h-10.5">
             <h1 className="text-snow text-lg font-bold ml-1.75">Synapse</h1>
