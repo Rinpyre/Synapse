5b125702e552731b0899de7cef550d355edb4eb5
Rinpyre <thecutefireboy@gmail.com>
2026-03-30 04:55:33 +0200

    refactor(LogsView): Remove temporary data files and update data fetching logic

--- frontend/src/routes/logsView.jsx
+++ frontend/src/routes/logsView.jsx
@@ -1,21 +1,22 @@
+import { useEffect, useState } from 'react'
 import { DataTable } from '@components/features'
-import rows from '@assets/tmp/rows.json'
-import columns from '@assets/tmp/columns.json'
-
-//? [TEMP] Multiply the rows to simulate a larger dataset (you can adjust the multiplier as needed)
-const multiplyRows = (rows, multiplier) => {
-    const multiplied = []
-    for (let i = 0; i < multiplier; i++) {
-        multiplied.push(...rows.map((row) => ({ ...row, id: row.id + i * rows.length })))
-    }
-    return multiplied
-}
 
 export const LogsViewPage = () => {
+    const [data, setData] = useState({ rows: [], columns: [] })
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:8000/api/logs')
+            const data = await response.json()
+            setData({ rows: data.rows, columns: data.columns })
+        }
+        fetchData()
+    }, [])
+
     return (
         <div className="logs-view-page flex h-full w-full flex-col items-center gap-2 p-8 pt-1.5">
             <h2 className="text-snow text-2xl font-bold">Logs View</h2>
-            <DataTable rows={multiplyRows(rows, 100)} columns={columns} />
+            <DataTable rows={data.rows} columns={data.columns} />
         </div>
     )
 }
