03d86d8e0cbfd20554ace4acf9c87b0a755c9180
Rinpyre <thecutefireboy@gmail.com>
2026-03-30 03:12:48 +0200

    fix(layout): Add overflow-auto to main content for better scrolling behavior

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ export const AppLayout = ({ children }) => {
     return (
         <div className="app bg-primary flex h-screen w-full">
             <Sidebar />
-            <div className="main-content flex grow flex-col">{children}</div>
+            <div className="main-content flex grow flex-col overflow-auto">{children}</div>
         </div>
     )
 }
