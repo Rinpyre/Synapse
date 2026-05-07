d74713f40ef47c1cd2b079a27aeb152a706b53b4
Rinpyre <thecutefireboy@gmail.com>
2026-05-07 14:51:43 +0200

    feat(LogController): Add detailed comments for filters parameter parsing and usage

--- backend/app/Http/Controllers/LogController.php
+++ backend/app/Http/Controllers/LogController.php
@@ -11,6 +11,20 @@ class LogController extends Controller
     {
         // Fetch logs from DB via Model
         // Paginated results for DataTable will be 15 by default, but be overridable via query params if needed
+        // TODO: Cache the "filters" parameter
+        /*
+         * Input: ?filters=school:harvard student:adrian changed time:13:45:39 date:2026-01-27
+         * Parse with urldecode and then split by space, then split by colon to get key-value pairs for filtering
+         * Example: filters=school:harvard student:adrian changed time:13:45:39 date:2026-01-27
+         * Output: [
+         *   'school' => 'harvard',
+         *   'student' => 'adrian',
+         *   'time' => '13:45:39',
+         *   'date' => '2026-01-27',
+         *   'freeText' => 'changed' // (if there are any terms that don't have a colon, treat them as free text search terms)
+         * ]
+         * Then apply these filters to the query builder for the Log model
+         */
         $perPage = (int) $request->query('per_page', 15);
         $perPage = max(1, min($perPage, 100));
         $logs = Log::orderBy('Time', 'desc')->paginate($perPage);
